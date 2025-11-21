692237014b4aafa87f4f1823e3ee367ea26e6245
alecrav <alessandrocravioglio@gmail.com>
2025-11-21 22:40:22 +0100

    begin to build single neuron network

---
+++ single-neuron-nn/src/main.rs
@@ -0,0 +1,14 @@
+use rand::prelude::*;
+
+pub(crate) struct Neuron {
+    inputs: Vec<u32>,
+    output: u32
+}
+
+fn main() {
+    // initialize inputs
+
+    // do stuff 
+
+    // store in output
+}
