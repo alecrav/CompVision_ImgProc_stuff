ad8ca5c985882294d5fe73079d7746300c77c98d
alecrav <alessandrocravioglio@gmail.com>
2025-12-09 23:52:42 +0100

    todo:verify if structure makes sense

--- single-neuron-nn/single_neuron/src/main.rs
+++ single-neuron-nn/single_neuron/src/main.rs
@@ -1,6 +1,8 @@
+use crate::neuron::{HiddenLayer, InputLayer, OutputLayer};
+
 mod neuron;
 
-fn train_epoch(neuron: &mut neuron::Neuron) -> f32 {
+fn train_epoch(neuron: &mut neuron::Neuron::<InputLayer>) -> f32 {
 
     let inputs = [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0];
     let ouptuts = [1.0,4.0,6.0,8.0,10.0,12.0,14.0,16.0];
@@ -22,27 +24,37 @@ fn train_epoch(neuron: &mut neuron::Neuron) -> f32 {
 }
 
 fn main() {
-    let mut neuron = neuron::Neuron {
+
+    // initialize 5 neurons: 2 first layer, 2 second layer, 1 third layer
+    let mut neuron_one = neuron::Neuron::<InputLayer> {
         weight: 0.1,
-        bias: 0.1
+        bias: 0.1,
+        _layer: std::marker::PhantomData,
     };
-    
+
+    let mut output_neuron = neuron:: Neuron::<OutputLayer> {
+        weight: 0.1,
+        bias: 0.1,
+        _layer: std::marker::PhantomData,
+    };
+
     for n in 0..50 {
         
-        let loss = train_epoch(&mut neuron);
+        let loss_one = train_epoch(&mut neuron_one);
+
         println!("---------- EPOCH {} ----------", n);
-        println!("{}, loss", loss);
-        println!("{}, bias", neuron.bias);
-        println!("{}, weight", neuron.weight);
+        println!("{}, loss", loss_one);
+        println!("{}, bias", neuron_one.bias);
+        println!("{}, weight", neuron_one.weight);
     }
 
     // predict something
     let input = 100.0;
-    let prediction = input * neuron.weight + neuron.bias;
+    let prediction = input * neuron_one.weight + neuron_one.bias;
     println!("prediction 1: {}", prediction);
 
 
     let input = 200.0;
-    let prediction = input * neuron.weight + neuron.bias;
+    let prediction = input * neuron_one.weight + neuron_one.bias;
     println!("prediction 2: {}", prediction);
 }
--- single-neuron-nn/single_neuron/src/neuron.rs
+++ single-neuron-nn/single_neuron/src/neuron.rs
@@ -1,11 +1,50 @@
-use std::cell::Cell;
+pub struct InputLayer;
+pub struct HiddenLayer;
+pub struct OutputLayer;
 
-pub struct Neuron {
+pub struct Neuron<T> {
     pub weight: f32,
     pub bias: f32,
+    pub _layer: std::marker::PhantomData<T>
 }
 
-impl Neuron {
+impl Neuron<HiddenLayer> {
+    pub fn train(&self, input: f32) -> f32 {
+        let output = (input * self.weight) + self.bias;
+        output
+    }
+
+    pub fn compute_loss(&self, output: f32, real_value: f32) -> f32 {
+        let loss = real_value - output;
+        loss
+    }
+
+    /// update weight and bias according to calculaed loss
+    pub fn backward_pass(&mut self, loss: f32, input: f32) {
+        self.weight = self.weight + (input * 0.01 * loss);
+        self.bias = self.bias + (0.01 * loss);
+    }
+}
+
+impl Neuron<InputLayer> {
+    pub fn train(&self, input: f32) -> f32 {
+        let output = (input * self.weight) + self.bias;
+        output
+    }
+
+    pub fn compute_loss(&self, output: f32, real_value: f32) -> f32 {
+        let loss = real_value - output;
+        loss
+    }
+
+    /// update weight and bias according to calculaed loss
+    pub fn backward_pass(&mut self, loss: f32, input: f32) {
+        self.weight = self.weight + (input * 0.01 * loss);
+        self.bias = self.bias + (0.01 * loss);
+    }
+}
+
+impl Neuron<OutputLayer> {
     pub fn train(&self, input: f32) -> f32 {
         let output = (input * self.weight) + self.bias;
         output
